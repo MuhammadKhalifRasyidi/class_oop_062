04a4ab997253fa27ab900bb0bcbfa312b3766b68
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-04-12 10:29:11 +0700

    Add project files.

---
+++ oop/oop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+private:
+	int nim;
+	string nama;
+	float nilai;
+public :
+	void input() {
+		cout << "Masukan NIM : ";
+		cin >> nim;
+		cout << "Masukan Nama : ";
+		cin >> nama;
+		cout << "Masukan Nilai : ";
+		cin >> nilai;
+	}
+	void printData();
+};
+
+void Mahasiswa::printData(){}
